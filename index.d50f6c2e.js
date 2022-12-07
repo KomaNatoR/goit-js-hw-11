556308e5010304bfd517fd9c2f2af5a96bc21531
KomaNatoR <108286639+KomaNatoR@users.noreply.github.com>
2022-12-07 03:55:22 +0000

    Deploying to gh-pages from @ KomaNatoR/goit-js-hw-11@1185fcd95d1036b29e57e0fc0b3b78133ee79808 🚀

---
+++ index.d50f6c2e.js
@@ -0,0 +1,2 @@
+document.querySelector(".search-form"),document.querySelector(".gallery");fetch("https://pixabay.com/api/?key=31888671-f215a97b976f323f834fb73b1&q=yellow+flowers&image_type=photo&orientation=horizontal&safesearch=true").then((e=>e.json())).then((function(e){console.log(e.hits)}));
+//# sourceMappingURL=index.d50f6c2e.js.map
